f1abde330c4cdd0fb7d912347799b43a8c2d63e4
Virlight <rotsehnsucht@gmail.com>
2023-01-22 16:17:20 +0100

    Commit by Haoliang Huang

--- odex/settings.py
+++ odex/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'odex.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE'  : 'django.db.backends.mysql',
+        'NAME'    : 'odexdb',
+        'USER'    : 'root',
+        'PASSWORD': 'odex123',
+        'HOST'    : 'localhost',
+        'PORT'    : '3306',
     }
 }
 
