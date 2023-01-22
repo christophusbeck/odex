97c74942944383528b106abc9ade5817a57d9b3f
Daonan <zdnzhangdaonan@gmail.com>
2023-01-22 15:46:15 +0100

    add my name

--- applications/views.py
+++ applications/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 
 # TODO: Sign-in Sheet
 #  please enter you name here, when you can connect gitlab
-#  Haoliang Huang, Chris(git + pyhcarm), Yuqi Cao
+#  Haoliang Huang, Chris(git + pyhcarm), Yuqi Cao, Daonan(pycharm only)
 
