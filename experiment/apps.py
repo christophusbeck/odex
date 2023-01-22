5421df0af9d7f956215a97f72eac30d71261aba3
dogc <caoyuqi1996@hotmail.com>
2023-01-22 22:48:30 +0100

    Modify apps.py in experiment and corresponding name of INSTALLED_APPS in settings.py

--- experiment/apps.py
+++ experiment/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class OdexConfig(AppConfig):
+class ExperimentConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'experiment'
