14b670bae2e1896b59563a256902a0e3801506c7
dogc <caoyuqi1996@hotmail.com>
2023-01-25 11:48:44 +0100

    Added comma at the end of last line in INSTALLED_APPS in settings.py

--- odex/settings.py
+++ odex/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'experiment.apps.ExperimentConfig',
-    'user.apps.UserConfig'
+    'user.apps.UserConfig',
 ]
 
 MIDDLEWARE = [
