d9acbf9c3e3bfed3fbcc4d8b443b5e38a1e60f00
dogc <caoyuqi1996@hotmail.com>
2023-01-26 20:49:13 +0100

    Added AboutUsView in views.py

--- user/views.py
+++ user/views.py
@@ -50,3 +50,14 @@ class DeleteAccountView(View):
 
     def post(self, request, *args, **kwargs):
         pass
+
+
+class AboutUsView(View):
+    template_name = ""  # waiting for html file
+
+    def get(self, request, *args, **kwargs):
+        pass
+
+    def post(self, request, *args, **kwargs):
+        pass
+
