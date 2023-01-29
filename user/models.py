b5443c65f73d8f3a66ff8690ecb4c5893d0c720b
Virlight <rotsehnsucht@gmail.com>
2023-01-29 16:40:23 +0100

    Commit by Haoliang Huang

--- user/models.py
+++ user/models.py
@@ -3,20 +3,23 @@ from django.db import models
 
 # Create your models here.
 
-class User(models.Model):
+class Users(models.Model):
     username = models.CharField(verbose_name="user_name", max_length=64)
     password = models.CharField(verbose_name="password", max_length=64)
+    tan = models.IntegerField(verbose_name="TAN")
 
-class SecurityAnswer(models.Model):
+
+class SecurityAnswers(models.Model):
     username = models.CharField(verbose_name="user_name", max_length=64)
     answer = models.CharField(verbose_name="user_name", max_length=64)
     id = models.CharField(verbose_name="questionId", max_length=10)
 
-class SecurityQuestion(models.Model):
+
+class SecurityQuestions(models.Model):
     id = models.SmallIntegerField(verbose_name="questionId", max_length=10)
     question = models.CharField(verbose_name="user_name", max_length=128)
 
 
-class TAN(models.Model):
+class TANs(models.Model):
     authenticated = models.BooleanField(verbose_name="authenticated", default=0)
-    code = models.CharField(verbose_name="TAN", max_length=128)
+    tan = models.CharField(verbose_name="TAN", max_length=128)
