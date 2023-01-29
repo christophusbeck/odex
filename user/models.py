dc6e2d28495eb1dd4bf6c8216a068b7c82a49e46
Virlight <rotsehnsucht@gmail.com>
2023-01-29 14:30:33 +0100

    Commit by Haoliang Huang

--- user/models.py
+++ user/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class UserInfo(models.Model):
     username = models.CharField(max_length=16, verbose_name="username")
     password = models.CharField(max_length=64, verbose_name="password")
-    tan = models.IntegerField(max_length=100000, verbose_name="TAN")
+    tan = models.IntegerField(verbose_name="TAN")
     create_time = models.DateTimeField(verbose_name="creating time")
 
     def __str__(self):
