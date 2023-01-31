8dcfb5649e4c98718f1e168ef6e2d6b79a74d3d3
dogc <caoyuqi1996@hotmail.com>
2023-02-01 00:41:35 +0100

    Changed {} to [] in urls.py of user and experiment to fix TypeError

--- user/urls.py
+++ user/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = {
+urlpatterns = [
     path('login/', views.LoginView.as_view()),
     path('signup/', views.RegistrationView.as_view()),
     path('logout/', views.LogOutView.as_view()),
     path('aboutus/', views.AboutUsView.as_view()),
     path('user/<int:nid>/delete/', views.DeleteAccountView.as_view()),
     path('test/', views.Test.as_view()),
-}
+]
