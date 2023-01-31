ea4f45250b8d0534b3bf130d298975c1b731af27
dogc <caoyuqi1996@hotmail.com>
2023-01-31 23:20:56 +0100

    Added templates paths to solve http 404 error

--- odex/settings.py
+++ odex/settings.py
@@ -61,7 +61,8 @@ ROOT_URLCONF = 'odex.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': []
+        'DIRS': [os.path.join(BASE_DIR, 'user', 'templates'),
+                 os.path.join(BASE_DIR, 'experiment', 'templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
