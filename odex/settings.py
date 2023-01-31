0d2bf72e44c65c8720d8ec0c6ac8ac5f12e69af5
dogc <caoyuqi1996@hotmail.com>
2023-01-31 02:21:22 +0100

    Modified .gitlab-ci.yml to fix problems of mysql connection

--- odex/settings.py
+++ odex/settings.py
@@ -80,15 +80,25 @@ WSGI_APPLICATION = 'odex.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE'  : 'django.db.backends.mysql',
+#         'NAME'    : os.environ.get('MYSQL_DB'),
+#         'USER'    : os.environ.get('MYSQL_USER'),
+#         'PASSWORD': os.environ.get('MYSQL_PASS'),
+#         'HOST'    : os.environ.get('MYSQL_HOST'),
+#         'PORT'    : '3306',
+#         'CONN_MAX_AGE':60,
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE'  : 'django.db.backends.mysql',
-        'NAME'    : os.environ.get('MYSQL_DB'),
-        'USER'    : os.environ.get('MYSQL_USER'),
-        'PASSWORD': os.environ.get('MYSQL_PASS'),
-        'HOST'    : os.environ.get('MYSQL_HOST'),
+        'NAME'    : 'odexdb',
+        'USER'    : 'root',
+        'PASSWORD': 'odex123',
+        'HOST'    : '127.0.0.1',
         'PORT'    : '3306',
-        'CONN_MAX_AGE':60,
     }
 }
 
