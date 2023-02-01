04d0b895c99790cd01cbce037606851c39458bb2
Virlight <rotsehnsucht@gmail.com>
2023-02-01 10:44:35 +0100

    modify ex model by Haoliang Huang

--- experiment/models.py
+++ experiment/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
 class Experiments(models.Model):
     userid = models.IntegerField(verbose_name="user id")
-    experiment_name = models.CharField(verbose_name="experiment name")
+    experiment_name = models.CharField(verbose_name="experiment nam", max_length=128)
