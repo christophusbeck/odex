5fcb36d61cd263c8a4851ba55158853ba8651be7
dogc <caoyuqi1996@hotmail.com>
2023-02-02 02:47:26 +0100

    Added a CheckUsername view for ajax real-time validation of unique username and added corresponding url in user.urls

--- user/urls.py
+++ user/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('login/', views.LoginView.as_view()),
     path('register/', views.RegistrationView.as_view()),
+    path('checkusername/', views.CheckUsername.as_view(), name='check_username'),
     path('logout/', views.LogOutView.as_view()),
     path('aboutus/', views.AboutUsView.as_view()),
     path('user/<int:nid>/delete/', views.DeleteAccountView.as_view()),
