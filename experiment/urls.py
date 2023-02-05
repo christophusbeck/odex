60ce037a337e5502cc63d7226ab7ece34dbb1c61
dogc <caoyuqi1996@hotmail.com>
2023-02-05 04:44:46 +0100

    Added experiment info and securityanswer info in fixtures.json

--- experiment/urls.py
+++ experiment/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('configuration/', views.Configuration.as_view()),
     path('login/',user.views.LoginView.as_view()),
     path('finishedDetail/',views.FinishedDetailView.as_view()),
-    path('pendingDetail/', views.PendingDeatilView.as_view()),
+    path('pendingDetail/', views.PendingDetailView.as_view()),
 ]
