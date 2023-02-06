fe6edb026d9bc537f5f99b8ce506a16cef2e55d1
Virlight <rotsehnsucht@gmail.com>
2023-02-06 08:25:16 +0100

    set display in config by Haoliang Huang

--- experiment/forms.py
+++ experiment/forms.py
@@ -14,29 +14,58 @@ class CreateForm(BootStrapModelForm):
         model = models.PendingExperiments
         fields = ["run_name", "main_file"]
 
-class ConfigForm(forms.Form):
+class ConfigForm(BootStrapModelForm):
 
-    ground_truth = fields.CharField(
-        widget= widgets.RadioSelect(choices= [(1, "detection only"), (2, "compare with a grund truth file"),]),
+    # ground_truth = fields.CharField(
+    #     widget= widgets.RadioSelect(choices= [(1, "detection only"), (2, "compare with a grund truth file"),]),
+    # )
+    #
+    # addtional_data = fields.CharField(
+    #     widget=widgets.CheckboxInput(),
+    # )
+    #
+    # operation_model = fields.CharField(
+    #     widget= widgets.RadioSelect(choices= [(1, "All subspaces"), (2, "All, except:"),(3, "Combination:"),]),
+    #     initial = 1
+    # )
+
+    class Meta:
+        model = models.PendingExperiments
+        fields = ['odm', 'operation', 'auxiliary_file_name', 'generated_file', 'ground_truth']
+        label = {
+            'operation': 'detected subspaces',
+            'generated_file': 'upload an additional data file',
+            'ground_truth': 'upload a ground truth file'
+        }
+
+    odm_options = fields.ChoiceField(
+        choices=[(1, "1"), (2, "2"), (3, "3"), ],
+        initial=2
     )
 
-    addtional_data = fields.CharField(
-        widget=widgets.CheckboxInput(),
+    ground_truth_options = fields.ChoiceField(
+        choices=[(1, "detection only"), (2, "compare with a grund truth file")],
+        widget=widgets.RadioSelect(attrs={'style':'width:30px; height:20px'})
     )
 
-    operation_model = fields.CharField(
-        widget= widgets.RadioSelect(choices= [(1, "All subspaces"), (2, "All, except:"),(3, "Combination:"),]),
-        initial = 1
+    additional_data_option = fields.ChoiceField(
+        widget=widgets.CheckboxInput(attrs={'style':'width:30px; height:20px'}),
+    )
+
+    operation_model_options = fields.ChoiceField(
+        choices=[(1, "All subspaces"), (2, "All, except:"), (3, "Combination:")],
+        widget=widgets.RadioSelect(attrs={'style':'width:30px; height:20px'}),
+        initial=1
     )
 
 
-class UpForm(BootStrapForm):
-    operation = forms.CharField(label="detected subspaces")
-    ground_truth = forms.FileField(label= "upload a ground truth file")
-    # still need to check if this is a valid file or not
-    add_data = forms.FileField(label= "upload an addtional data file")
-    # upload all the informations in a table! only for test
-    # parameter = forms.CharField(label="parameters")
+# class UpForm(BootStrapForm):
+#     operation = forms.CharField(label="detected subspaces")
+#     ground_truth = forms.FileField(label= "upload a ground truth file")
+#     # still need to check if this is a valid file or not
+#     add_data = forms.FileField(label= "upload an addtional data file")
+#     # upload all the informations in a table! only for test
+#     # parameter = forms.CharField(label="parameters")
 
 
 class BootStrapForm(forms.Form):
--- experiment/views.py
+++ experiment/views.py
@@ -8,7 +8,7 @@ from django.shortcuts import render, redirect
 from django.views import View
 
 import tools.odm_handling
-from experiment.forms import CreateForm, ConfigForm, UpForm
+from experiment.forms import CreateForm, ConfigForm
 from experiment import models
 
 from django.http import JsonResponse, HttpResponse
@@ -113,7 +113,9 @@ class MainView(View):
 class DeleteView(View):
     def get(self, request, *args, **kwargs):
         print(request.GET['id'])
+        print(models.Users.objects.all())
         models.Experiments.objects.filter(id=request.GET['id']).delete()
+        print(models.Users.objects.all())
         return redirect("/main/")
 
 
@@ -124,19 +126,17 @@ class Configuration(View):
 
         # only for test
         # exp_Info = models.Experiments.objects.all()
-        exp_Info = models.Experiments.objects.filter(id=request.GET['id'])
+        exp = models.Experiments.objects.filter(id=request.GET['id'])
+        columns = exp[0].get_columns()
         form = ConfigForm()
-        upform = UpForm()
-        # print(request.GET)
         odms = tools.odm_handling.get_odm_dict().keys()
         print("here is odems: ", dict(tools.odm_handling.get_def_value_dict(tools.odm_handling.match_odm_by_name("ABOD"))).keys())
-        return render(request, self.template_name, {"exp_Info": exp_Info, "form": form, "upform":upform, "odms":odms})
+        return render(request, self.template_name, {"exp": exp, "columns": columns, "form": form, "odms":odms})
 
     def post(self, request, *args, **kwargs):
-        form = UpForm(data=request.POST, files=request.FILES)
+        form = ConfigForm(data=request.POST, files=request.FILES)
 
         exp_Info = models.Experiments.objects.all()
-        upform = UpForm()
         formConf = ConfigForm()
 
         if form.is_valid():
