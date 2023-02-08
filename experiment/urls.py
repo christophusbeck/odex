36ab6918c6f5fe1a4547d20c4f467f48011f73da
Daonan <zdnzhangdaonan@gmail.com>
2023-02-09 00:31:58 +0100

    add page functions in the main page

--- experiment/urls.py
+++ experiment/urls.py
@@ -3,14 +3,14 @@ from django.urls import path
 import user.views
 from . import views
 
-
 urlpatterns = [
     path('main/', views.MainView.as_view()),
     path('delete/', views.DeleteView.as_view()),
     path('configuration/', views.Configuration.as_view()),
-    path('aboutus/',user.views.AboutUsView.as_view()),
-    path('login/',user.views.LoginView.as_view()),
-    path('finishedDetail/',views.FinishedDetailView.as_view()),
+    path('aboutus/', user.views.AboutUsView.as_view()),
+    path('login/', user.views.LoginView.as_view()),
+    path('finishedDetail/', views.FinishedDetailView.as_view()),
     path('pendingDetail/', views.PendingDetailView.as_view()),
     path('result/', views.ResultView.as_view()),
+    # path('refresh/', views.refreshView.as_view(), name='username'),
 ]
