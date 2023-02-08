42d0a8b0ee29886be240f616e481ad908014c5c4
Christoph Henry Usbeck <uouvn@student.kit.edu>
2023-02-08 18:20:18 +0100

    Addded metrics generation to detector_thread.py

--- tools/detector_thread.py
+++ tools/detector_thread.py
@@ -1,4 +1,5 @@
 import threading
+from django.utils import timezone
 
 from experiment import models
 from tools import odm_handling
@@ -30,6 +31,9 @@ class DetectorThread(threading.Thread):
 
             print("outlier classification: ", outlier_classification)
 
+            metrics = {}
+            metrics["Detected Outliers"] = sum(outlier_classification)
+
 
             result_csv = []
             result_csv.append(user_csv[1])
@@ -44,6 +48,14 @@ class DetectorThread(threading.Thread):
                 ground_truth_array = odm_handling.get_array_from_csv_data(ground_truth_csv)
 
                 (tp, fp, fn, tn) = odm_handling.calculate_confusion_matrix(outlier_classification, ground_truth_array)
+                metrics["True positives"] = tp
+                metrics["False positives"] = fp
+                metrics["True negatives"] = tn
+                metrics["False positives"] = fn
+
+                metrics["Precision"] = tp / (tp + fp)
+                metrics["Recall"] = tp / (tp + fn)
+
             if exp.generated_file != "":
                 user_gen_csv = odm_handling.get_data_from_csv(exp.generated_file)
                 user_gen_data = odm_handling.get_array_from_csv_data(user_gen_csv[1:])
@@ -51,8 +63,15 @@ class DetectorThread(threading.Thread):
                 clf_merge = exp_odm(**exp_para)
                 clf_merge.fit(self, merged_data)
                 outlier_classification_after_merge = clf_merge.predict(self, merged_data)
+                metrics["Detected Outliers after merging with generated data"] = sum(outlier_classification_after_merge)
 
-            # exp = models.FinishedExperiments()
+            duration = exp.start_time - timezone.now()
+            print(metrics)
+
+            f_exp = models.FinishedExperiments(exp, metrics, result_csv_path, duration)
+
+
+
 
         except Exception as e:
             print(e)
