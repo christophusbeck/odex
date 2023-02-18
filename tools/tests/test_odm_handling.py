f21a97d5703a1c3c11c8d55d3a1b8ed0968e3187
Christoph Henry Usbeck <uouvn@student.kit.edu>
2023-02-18 23:45:46 +0100

    Added tests for odm_handling

--- tools/tests/test_odm_handling.py
+++ tools/tests/test_odm_handling.py
@@ -1,3 +1,6 @@
+import os
+import random
+import re
 import unittest
 from tools import odm_handling
 import numpy as np
@@ -5,6 +8,121 @@ import numpy as np
 class Test_odm_handling(unittest.TestCase):
 
     def setup(self):
+        pass
+
+
+    def test_get_array_from_csv_data(self):
+        pass
+
+    def test_subspace_exclusion_check(self):
+        max_col = random.randint(2, 10)
+
+        empty_string = odm_handling.subspace_exclusion_check("", max_col)
+        nonsensical_string = odm_handling.subspace_exclusion_check("not a valid input", max_col)
+        exclude_negative = odm_handling.subspace_exclusion_check("-1", max_col)
+        exclude_beyond_max = odm_handling.subspace_exclusion_check(str(max_col + 1), max_col)
+        valid_picks_list = odm_handling.subspace_exclusion_check(str(max_col - 1), max_col)
+
+        self.assertFalse(empty_string)
+        self.assertFalse(nonsensical_string)
+        self.assertFalse(exclude_negative)
+        self.assertFalse(exclude_beyond_max)
+        self.assertTrue(len(valid_picks_list) > 0)
+
+    def test_subspace_combination_check(self):
+        max_col = random.randint(2, 10)
+
+        empty_string = odm_handling.subspace_combination_check("", max_col)
+        nonsensical_string = odm_handling.subspace_combination_check("not a valid input", max_col)
+        include_negative = odm_handling.subspace_combination_check("{-1}", max_col)
+        include_beyond_max = odm_handling.subspace_combination_check("{" + str(max_col + 1) + "}", max_col)
+
+        valid_input = odm_handling.subspace_combination_check("{1,2}", max_col)
+        valid_input_and = odm_handling.subspace_combination_check("{1,2}&{1}", max_col)
+        valid_input_or = odm_handling.subspace_combination_check("{1,2}|{1}", max_col)
+        valid_input_and_or = odm_handling.subspace_combination_check("{1,2}&{1}|{1,2}", max_col)
+
+        self.assertFalse(empty_string)
+        self.assertFalse(nonsensical_string)
+        self.assertFalse(include_negative)
+        self.assertFalse(include_beyond_max)
+
+        self.assertTrue(valid_input)
+        self.assertTrue(valid_input_and)
+        self.assertTrue(valid_input_or)
+        self.assertTrue(valid_input_and_or)
+
+    def test_subspace_selection_parser(self):
+        pass
+
+    def test_get_head_indexing(self):
+
+        data = np.random.randint(-1000, 1000, (100, 100))
+        indexing = odm_handling.get_head_indexing(data)
+        headrow = data[0, :]
+
+        for index_tuple in indexing:
+            (val, i) = index_tuple
+            self.assertEqual(index_tuple, [headrow[int(i)], str(i)])
+
+    def test_get_def_value_dict(self):
+        pass
+
+    def test_get_odm_dict(self):
+        odm_dict = odm_handling.get_odm_dict()
+        self.assertTrue(len(odm_dict) > 0)
+
+    def test_match_odm_by_name(self):
+        odm_dict = odm_handling.get_odm_dict()
+        pick = random.choice(list(odm_dict.values()))
+
+        odm_name = str(pick)
+        odm_name = odm_name.replace("<class '", "")
+        odm_name = odm_name.replace("'>", "")
+        odm_name = odm_name.split(".")[3]
+
+        self.assertEqual(pick, odm_dict[odm_name])
+
+    def test_calculate_confusion_matrix(self):
+        pred = np.random.randint(0, 1, 100)
+        actual = pred = np.random.randint(0, 1, 100)
+
+        (tp_test, fn_test, fp_test, tn_test) = odm_handling.calculate_confusion_matrix(pred, actual)
+
+        tp = 0
+        fp = 0
+        tn = 0
+        fn = 0
+        i = 0
+        for datapoint in pred:
+            if pred[i] and actual[i]:
+                tp += 1
+            elif pred[i] and (not actual[i]):
+                fp += 1
+            elif (not pred[i]) and (not actual[i]):
+                tn += 1
+            elif (not pred[i]) and actual[i]:
+                fn += 1
+            i += 1
+
+        self.assertEqual(tp, tp_test)
+        self.assertEqual(fp, fp_test)
+        self.assertEqual(tn, tn_test)
+        self.assertEqual(fn, fn_test)
+
+    def test_col_subset(self):
+        data = np.random.randint(-1000, 1000, (100, 100))
+        selected_col = np.random.randint(0, 99)
+
+        subset = odm_handling.col_subset(data, [selected_col])
+
+        i = 0
+        for orig_val in data[:, selected_col]:
+            self.assertEqual([orig_val], subset[i])
+            i += 1
+
+
+    def test_operate_and_on_arrays(self):
         pass
 
     def test_operate_or_on_arrays(self):
@@ -16,39 +134,6 @@ class Test_odm_handling(unittest.TestCase):
 
         #for (a, b, c) in (or_array_1, or_array_2, or_array_result):
             #self.assertEqual(c, a or b)
-        pass
-
-    def test_get_array_from_csv_data(self):
-        pass
-
-    def test_subspace_exclusion_check(self):
-        pass
-
-    def test_subspace_combination_check(self):
-        pass
-
-    def test_subspace_selection_parser(self):
-        pass
-
-    def test_get_head_indexing(self):
-        pass
-
-    def test_get_def_value_dict(self):
-        pass
-
-    def test_get_odm_dict(self):
-        pass
-
-    def test_match_odm_by_name(self):
-        pass
-
-    def test_calculate_confusion_matrix(self):
-        pass
-
-    def test_col_subset(self):
-        pass
-
-    def test_operate_and_on_arrays(self):
         pass
 
     def test_csv_write_and_read(self):
@@ -64,6 +149,7 @@ class Test_odm_handling(unittest.TestCase):
             write_data_str.append(new_row)
 
         self.assertEqual(write_data_str, read_data)
+        os.remove(path)
 
 
 if __name__ == '__main__':
