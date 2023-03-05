e8fb13ffd48a16854f254855b50cf405c36b3149
dogc <caoyuqi1996@hotmail.com>
2023-03-05 03:50:31 +0100

    test(user.urls): added url name for testing url

--- user/urls.py
+++ user/urls.py
@@ -3,15 +3,15 @@ from . import views
 
 
 urlpatterns = [
-    path('login/', views.LoginView.as_view()),
-    path('register/', views.RegistrationView.as_view()),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('register/', views.RegistrationView.as_view(), name='register'),
     path('checkusername/', views.CheckUsername.as_view(), name='check_username'),
-    path('logout/', views.LogOutView.as_view()),
+    path('logout/', views.LogOutView.as_view(), name='logout'),
     # after user clicks logout, he/she should return to login page
-    path('user/delete/', views.DeleteAccountView.as_view()),
-    path('resetpassword/', views.ResetPasswordView.as_view()),
+    path('user/delete/', views.DeleteAccountView.as_view(), name='delete'),
+    path('resetpassword/', views.ResetPasswordView.as_view(), name='reset_password'),
     #path('forgetpassword/', views.ForgetPasswordView.as_view()),
-    path('changename/', views.ChangeNameView.as_view()),
-    path('changepassword/', views.ChangePasswordView.as_view())
+    path('changename/', views.ChangeNameView.as_view(), name='change_name'),
+    path('changepassword/', views.ChangePasswordView.as_view(), name='change_password')
     # delete account also like logout, the only difference is that, the count should be deleted(according to suffix nid) and after that get into login page
 ]
