16ac3e2483e92648f1953f5529bf6e35eaf97662
dogc <caoyuqi1996@hotmail.com>
2023-03-05 13:02:28 +0100

    test(user.views.RegistrationView): T1.4 used tan code

--- user/tests/test_views.py
+++ user/tests/test_views.py
@@ -22,11 +22,13 @@ class RegistrationViewTest(TestCase):
         self.response_post = self.client.post(self.url, data, follow=True)
 
     '''--------------------------- Test Fixture Loading ---------------------------'''
+
     def test_fixtures(self):
         tan = models.TANs.objects.get(id=1)
         self.assertEqual(tan.tan, '123')
 
     '''---------------------------   Basic URL tests    ---------------------------'''
+
     def test_page_status_code(self):
         self.assertEqual(self.response_get.status_code, 200)
 
@@ -43,6 +45,7 @@ class RegistrationViewTest(TestCase):
         self.assertIsInstance(form, forms.RegisterForm)
 
     '''--------------------------- Successful Registration ---------------------------'''
+
     def test_redirection(self):
         self.assertRedirects(self.response_post, self.successful_url, status_code=302, target_status_code=200)
 
@@ -63,3 +66,22 @@ class RegistrationViewTest(TestCase):
         answer = models.SecurityAnswers.objects.filter(user=user).first()
         question = models.SecurityQuestions.objects.get(id=answer.question.id)
         self.assertEqual(question.id, 2)
+
+    '''--------------------------- Failed Registration ---------------------------'''
+
+    def test_empty_form(self):
+        response = self.client.post(self.url, {})
+        self.assertEqual(response.status_code, 200)
+
+    def test_used_tan(self):
+        data = {
+            'username': 'new user',
+            'password': '123456',
+            'repeat_password': '123456',
+            'tan': '124',
+            'question': 2,
+            'answer': 'cat'
+        }
+        response = self.client.post(self.url, data)
+        form = response.context.get('form')
+        self.assertEqual(str(form.errors['tan'][0]), "tan is used")
