ade9a91ab908727c67ce240aa34301d89aebc1fb
Christoph Henry Usbeck <uouvn@student.kit.edu>
2023-03-05 15:41:49 +0100

    utest(tools.odm_handling.subspace_exclusion_check): added wrongful inut case for excluding more columns than existing

--- tools/tests/test_odm_handling.py
+++ tools/tests/test_odm_handling.py
@@ -31,12 +31,14 @@ class Test_odm_handling(TestCase):
         exclude_negative = odm_handling.subspace_exclusion_check("-1", max_col)
         exclude_beyond_max = odm_handling.subspace_exclusion_check(str(max_col + 1), max_col)
         valid_picks_list = odm_handling.subspace_exclusion_check(str(max_col - 1), max_col)
+        exclude_more_than_possible = odm_handling.subspace_exclusion_check("1,2,3", 0)
 
         self.assertFalse(empty_string)
         self.assertFalse(nonsensical_string)
         self.assertFalse(exclude_negative)
         self.assertFalse(exclude_beyond_max)
         self.assertTrue(len(valid_picks_list) > 0)
+        self.assertFalse(exclude_more_than_possible)
 
     def test_subspace_combination_check(self):
         max_col = random.randint(2, 10)
