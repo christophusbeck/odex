7c698231a727d3e3894a4b14184d311c93a93f45
Christoph Henry Usbeck <uouvn@student.kit.edu>
2023-03-06 23:03:08 +0100

    Created test class for detector_thread.py

---
+++ experiment/tests/test_detector_thread.py
@@ -0,0 +1,15 @@
+import unittest
+
+import pyod.models.abod
+from django.test import TestCase
+import numpy as np
+
+
+class Test_detector_thread(TestCase):
+
+    def setup(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
