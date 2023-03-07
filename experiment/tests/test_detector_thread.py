5fa2d08e68d71af1d47c2b0b0576ab8393fabf57
Christoph Henry Usbeck <uouvn@student.kit.edu>
2023-03-07 22:05:02 +0100

    utest(tools.detector_thread.run): extended test

--- experiment/tests/test_detector_thread.py
+++ experiment/tests/test_detector_thread.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 import random
 
@@ -19,24 +20,35 @@ class Test_detector_thread(TestCase):
         pass
 
     def test_run(self):
+        path_media = "media\\"
         path_input = "input.csv"
+        path_gt = "gr.csv"
+        path_gen = "gen.csv"
         user = models.Users.objects.create(id=0)
 
         #exp = PendingExperiments()
-        exp = models.PendingExperiments.objects.create(user_id=0, id=0)
+        exp = models.PendingExperiments.objects.create(user=user, id=0)
         exp.id = 0
         odm_pick = "ABOD" #random.choice(list(odm_handling.get_odm_dict().keys()))
         exp.odm = odm_handling.match_odm_by_name(odm_pick)
         exp.set_para(odm_handling.get_def_value_dict(exp.odm).copy())
-        print(exp.get_para())
+        #print(exp.get_para())
         exp.operation = "1"
 
         training_data, test_data, train_gt, test_gt = pyod.utils.data.generate_data(400, 100, 3, 0.1)
-        odm_handling.write_data_to_csv(path_input, training_data)
-        exp.main_file = FileField(upload_to=path_input)
+        odm_handling.write_data_to_csv(path_media + path_input, training_data)
+        odm_handling.write_data_to_csv(path_media + path_gt, train_gt)
+        odm_handling.write_data_to_csv(path_media + path_gen, test_data)
+
+        exp.main_file.name = path_input
+        exp.save()
 
         det_thread = detector_thread.DetectorThread(id=0)
         det_thread.run()
+
+        os.remove(path_media + path_input)
+        os.remove(path_media + path_gt)
+        os.remove(path_media + path_gen)
 
 
 
