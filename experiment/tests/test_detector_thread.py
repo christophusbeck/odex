a93070d431816888ec7ebaab4fdc55538e63c459
Christoph Henry Usbeck <uouvn@student.kit.edu>
2023-03-08 22:49:58 +0100

    utest(tools.detector_thread.run): moved reoccuring code into its own method

--- experiment/tests/test_detector_thread.py
+++ experiment/tests/test_detector_thread.py
@@ -27,9 +27,8 @@ class Test_detector_thread(TestCase):
     def tearDown(self):
         pass
 
-    def test_run_only_od(self):
+    def __run_detector_thread(self, use_gt=False, use_gen=False, operation_option="1"):
         training_data, test_data, train_gt, test_gt = pyod.utils.data.generate_data(401, 100, 3, 0.1)
-
 
         gt_list = []
         for row in train_gt:
@@ -43,29 +42,44 @@ class Test_detector_thread(TestCase):
         if not os.path.exists("media/user_0/0/"):
             os.mkdir("media/user_0/0/")
 
-
         user = models.Users.objects.create(id=0)
         exp = models.PendingExperiments.objects.create(user=user, id=0)
         exp.id = 0
-        odm_pick = "ABOD"  # random.choice(list(odm_handling.get_odm_dict().keys()))
+        odm_pick = random.choice(list(odm_handling.get_odm_dict().keys()))
+        print("odm_pick = ", odm_pick)
         exp.odm = odm_handling.match_odm_by_name(odm_pick)
-        exp.set_para(odm_handling.get_def_value_dict(exp.odm).copy())
-        exp.operation = "1"
+        print(exp.odm)
+        exp.set_para({})
+        exp.operation = operation_option
+        exp.operation_option = operation_option
         exp.start_time = timezone.now()
-        exp.operation_option = "1"
         exp.run_name = "test_exp"
 
         exp.main_file.name = self.path_input
         exp.file_name = self.path_input
-        exp.save()
 
-        #odm_handling.write_data_to_csv("media/user_0/0/result_" +self.path_input, [[]])
+        if use_gt:
+            exp.ground_truth = self.path_gt
+        if use_gen:
+            exp.generated_file = self.path_gen
+        exp.save()
 
         det_thread = detector_thread.DetectorThread(id=0)
         det_thread.run()
-
         self.assertTrue(os.path.exists(self.path_outputs + "metrics_" + self.path_input))
         self.assertTrue(os.path.exists(self.path_outputs + "result_" + self.path_input))
+        if use_gen:
+            self.assertTrue(os.path.exists(self.path_outputs + "result_" +
+                                           self.path_input.replace(".csv") + "_with_addition.csv"))
+
+        os.remove(self.path_outputs + "metrics_" + self.path_input)
+        os.remove(self.path_outputs + "result_" + self.path_input)
+
+    def test_run_only_od(self):
+        self.__run_detector_thread(False, False, "1")
+
+    def test_run_with_gt(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()
