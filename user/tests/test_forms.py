b912aa41afd3652b71a27217eca1737d243ec770
Ao Zuo <unobm@student.kit.edu>
2023-03-12 12:28:52 +0100

    <utest>user/test/test_form.py:<edit RegisterFormTestTest>

--- user/tests/test_forms.py
+++ user/tests/test_forms.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
-from user.models import Users, SecurityQuestions
-from user.forms import LoginForm, QuestionForm, RegisterForm
-
+from user.models import Users, SecurityQuestions,SecurityAnswers,TANs
+from user.forms import LoginForm, QuestionForm, RegisterForm,InitialResetForm
+from tools.encrypt import md5
 
 class LoginFormTest(TestCase):
     def test_login_form_valid(self):
@@ -26,18 +26,67 @@ class QuestionFormTest(TestCase):
         form = QuestionForm(data={})
         self.assertFalse(form.is_valid())
 
+
 class RegisterFormTest(TestCase):
     def test_register_form_valid(self):
         security_question = SecurityQuestions.objects.create(
             question='What is your favorite color?'
         )
         form_data = {
-            'username': 'testuser',
-            'password': 'testpassword',
-            'repeat_password': 'testpassword',
+            'username': 'tester',
+            'password': '123',
+            'repeat_password': '123',
             'tan': '123',
             'question': security_question.id,
             'answer': 'blue'
         }
         form = RegisterForm(data=form_data)
         self.assertTrue(form.is_valid())
+
+    def test_register_form_inconsistent_password(self):
+        security_question = SecurityQuestions.objects.create(
+            question='What is your favorite color?'
+        )
+        form_data = {
+            'username': 'tester',
+            'password': '123',
+            'repeat_password': '456',
+            'tan': '123',
+            'question': security_question.id,
+            'answer': 'blue'
+        }
+        form = RegisterForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(str(form.errors['__all__'][0]),'Inconsistent password input')
+
+    def test_register_form_clean_repeat_password(self):
+        security_question = SecurityQuestions.objects.create(
+            question='What is your favorite color?'
+        )
+        form_data = {
+            'username': 'tester1',
+            'password': '123',
+            'repeat_password': '123',
+            'tan': '123',
+            'question': security_question.id,
+            'answer': 'blue'
+        }
+        form = RegisterForm(data=form_data)
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.cleaned_data['repeat_password'], '7088966d4c03aeb6a66c390ec6462589')
+
+    def test_register_form_clean_password(self):
+        security_question = SecurityQuestions.objects.create(
+            question='What is your favorite color?'
+        )
+        form_data = {
+            'username': 'tester1',
+            'password': '123',
+            'repeat_password': '123',
+            'tan': '123',
+            'question': security_question.id,
+            'answer': 'blue'
+        }
+        form = RegisterForm(data=form_data)
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.cleaned_data['password'], '7088966d4c03aeb6a66c390ec6462589')
