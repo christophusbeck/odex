db4c33a7ba5badd0beb005815783fbe86b222a5e
Ao Zuo <unobm@student.kit.edu>
2023-03-12 12:46:33 +0100

    <utest>user/test/test_form.py:<edit ResetPasswordFormTest,ChangePasswordFormTest>

--- user/tests/test_forms.py
+++ user/tests/test_forms.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
-from user.models import Users, SecurityQuestions,SecurityAnswers,TANs
-from user.forms import LoginForm, QuestionForm, RegisterForm,InitialResetForm
-from tools.encrypt import md5
+from user.models import Users, SecurityQuestions, SecurityAnswers, TANs
+from user.forms import LoginForm, QuestionForm, RegisterForm, ResetPasswordForm, ChangePasswordForm
+
 
 class LoginFormTest(TestCase):
     def test_login_form_valid(self):
@@ -57,7 +57,7 @@ class RegisterFormTest(TestCase):
         }
         form = RegisterForm(data=form_data)
         self.assertFalse(form.is_valid())
-        self.assertEqual(str(form.errors['__all__'][0]),'Inconsistent password input')
+        self.assertEqual(str(form.errors['__all__'][0]), 'Inconsistent password input')
 
     def test_register_form_clean_repeat_password(self):
         security_question = SecurityQuestions.objects.create(
@@ -90,3 +90,63 @@ class RegisterFormTest(TestCase):
         form = RegisterForm(data=form_data)
         self.assertTrue(form.is_valid())
         self.assertEqual(form.cleaned_data['password'], '7088966d4c03aeb6a66c390ec6462589')
+
+
+class ResetPasswordFormTest(TestCase):
+    def test_resetpassword_form_valid_data(self):
+        form_data = {
+            'password': '123',
+            'repeat_password': '123',
+            'answer': 'mysecurityanswer'
+        }
+        form = ResetPasswordForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_resetpassword_form_invalid_passwords(self):
+        form_data = {
+            'password': '123',
+            'repeat_password': '456',
+            'answer': 'mysecurityanswer'
+        }
+        form = ResetPasswordForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+    def test_resetpassword_form_invalid_answer(self):
+        form_data = {
+            'password': '123',
+            'repeat_password': '123',
+            'answer': ''
+        }
+        form = ResetPasswordForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+
+class ChangePasswordFormTest(TestCase):
+    def setUp(self):
+        self.valid_data = {
+            'new_password': '123',
+            'repeat_password': '123',
+        }
+        self.invalid_data = {
+            'new_password': '123',
+            'repeat_password': '456',
+        }
+
+    def test_valid_data(self):
+        form = ChangePasswordForm(data=self.valid_data)
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_data(self):
+        form = ChangePasswordForm(data=self.invalid_data)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(str(form.errors['__all__'][0]), 'Inconsistent password input')
+
+    def test_clean_repeat_password(self):
+        form = ChangePasswordForm(data=self.valid_data)
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.cleaned_data['repeat_password'], '7088966d4c03aeb6a66c390ec6462589')
+
+    def test_clean_new_password(self):
+        form = ChangePasswordForm(data=self.valid_data)
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.cleaned_data['new_password'], '7088966d4c03aeb6a66c390ec6462589')
