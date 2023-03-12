8bf5e4bb57e62837c51b233106f1be202df5d1f0
uqqww <daonan.zhang@student.kit.edu>
2023-03-12 19:58:26 +0100

    add some unit tests for experiment list view

--- experiment/tests/test_views.py
+++ experiment/tests/test_views.py
@@ -1,10 +1,13 @@
 
+from datetime import timedelta
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase, Client, RequestFactory
 from django.urls import reverse
+from django.utils.timezone import now
 
 import experiment.models
 import user
+from experiment.views import ResultView
 from user import models
 import json
 
@@ -84,21 +87,42 @@ class Test_DeleteView(TestCase):
 class Test_ExperimentlistView(TestCase):
     def setUp(self):
         '''--------------------------- logged in ---------------------------'''
-        user = Users.objects.create(username='tester3', password='123')
+        self.user = Users.objects.create(username='tester3', password='123')
         session = self.client.session
-        session['info'] = {'id': user.id, 'username': user.username}
+        session['info'] = {'id': self.user.id, 'username': self.user.username}
         session.save()
         self.client.post('/login/')
         self.url = reverse('main')
         self.response_get = self.client.get(self.url)
+
+        self.experiment = experiment.models.Experiments.objects.create(
+            user=self.user, run_name='Test Run', created_time=now(),
+            state='pending', file_name='test.csv', operation='filter', start_time=now(),
+            duration=timedelta(minutes=10))
 
     def test_GET(self):
         # not allowed to get explist as url
         response = self.client.get(reverse('explist'))
         self.assertEqual(response.status_code, 405)
 
+    def test_post(self):
+        response = self.client.post(reverse('explist'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response['content-type'], 'application/json')
+        self.assertIn('total', response.json())
+        self.assertIn('rows', response.json())
 
 
-
-
-
+    def test_row_generator(self):
+        rows = experiment.views.ExperimentListView().row_generator(experiment.models.Experiments.objects.all())
+        self.assertIsInstance(rows, list)
+        self.assertIsInstance(rows[0], dict)
+        self.assertIn('id', rows[0])
+        self.assertIn('run_name', rows[0])
+        self.assertIn('created_time', rows[0])
+        self.assertIn('state', rows[0])
+        self.assertIn('odm', rows[0])
+        self.assertIn('file_name', rows[0])
+        self.assertIn('operation', rows[0])
+        self.assertIn('start_time', rows[0])
+        self.assertIn('duration', rows[0])
