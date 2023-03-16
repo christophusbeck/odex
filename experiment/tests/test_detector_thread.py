6566abe64d9bc39d090d398c9e937df6d041401f
Christoph Henry Usbeck <uouvn@student.kit.edu>
2023-03-16 14:56:21 +0100

    utest(tools.detector_thread.run): restructured tests

--- experiment/tests/test_detector_thread.py
+++ experiment/tests/test_detector_thread.py
@@ -1,4 +1,5 @@
 import os
+import time
 import unittest
 import random
 
@@ -25,7 +26,21 @@ class Test_detector_thread(TestCase):
         pass
 
     def tearDown(self):
+        self.__try_remove_file(self.path_outputs + "metrics_" + self.path_input)
+        self.__try_remove_file(self.path_outputs + "result_" + self.path_input)
+        self.__try_remove_file(self.path_outputs + "result_" + self.path_input.replace(".csv", "") + "_with_addition.csv")
+        self.__try_remove_file(self.path_media + self.path_gt)
+        self.__try_remove_file(self.path_media + self.path_gen)
+        self.__try_remove_file(self.path_media + "input_roc.jpg")
+        self.__try_remove_file(self.path_media + "gen_roc.jpg")
         pass
+
+    def __try_remove_file(self, path):
+        if os.path.exists(path):
+            try:
+                os.remove(path)
+            except Exception as e:
+                pass
 
     def __run_detector_thread(self, use_gt=False, use_gen=False, operation_option="1"):
         training_data, test_data, train_gt, test_gt = pyod.utils.data.generate_data(400, 100, 3, 0.1)
@@ -78,6 +93,7 @@ class Test_detector_thread(TestCase):
 
         det_thread = detector_thread.DetectorThread(id=0)
         det_thread.run()
+        time.sleep(1.0)
 
         if not use_gen:
             self.assertTrue(os.path.exists(self.path_outputs + "metrics_" + self.path_input))
@@ -86,48 +102,54 @@ class Test_detector_thread(TestCase):
         if use_gen:
             self.assertTrue(os.path.exists(path_gen_res))
 
+        path_input_roc = self.path_media + "input_roc.jpg"
         if use_gt:
-            path_input_roc = self.path_media + "input_roc.jpg"
             self.assertTrue(os.path.exists(path_input_roc))
+
+        path_gen_roc = self.path_media + "gen_roc.jpg"
         if use_gt and use_gen:
-            path_geb_roc = self.path_media + "gen_roc.jpg"
-            self.assertTrue(os.path.exists(path_geb_roc))
+            self.assertTrue(os.path.exists(path_gen_roc))
 
         models.Users.objects.all().delete()
         models.PendingExperiments.objects.all().delete()
 
-        try:
-            os.remove(self.path_outputs + "metrics_" + self.path_input)
-            os.remove(self.path_outputs + "result_" + self.path_input)
-            os.remove(path_gen_res)
-            os.remove(self.path_media + self.path_gt)
-            os.remove(self.path_media + self.path_gen)
-        except Exception as e:
-            pass
 
-    def test_run_only_od(self):
+
+    def test_run_only_od_1(self):
         self.__run_detector_thread(False, False, "1")
+
+    def test_run_only_od_2(self):
         self.__run_detector_thread(False, False, "2")
+
+    def test_run_only_od_3(self):
         self.__run_detector_thread(False, False, "3")
-        pass
 
-    def test_run_with_gt(self):
+    def test_run_with_gt_1(self):
         self.__run_detector_thread(True, False, "1")
+
+    def test_run_with_gt_2(self):
         self.__run_detector_thread(True, False, "2")
+
+    def test_run_with_gt_3(self):
         self.__run_detector_thread(True, False, "3")
-        pass
 
-    def test_run_with_gen(self):
+    def test_run_with_gen_1(self):
         self.__run_detector_thread(False, True, "1")
-        self.__run_detector_thread(False, True, "2")
-        self.__run_detector_thread(False, True, "3")
-        pass
 
-    def test_run_with_gt_and_gen(self):
+    def test_run_with_gen_2(self):
+        self.__run_detector_thread(False, True, "2")
+
+    def test_run_with_gen_3(self):
+        self.__run_detector_thread(False, True, "3")
+
+    def test_run_with_gt_and_gen_1(self):
         self.__run_detector_thread(True, True, "1")
+
+    def test_run_with_gt_and_gen_2(self):
         self.__run_detector_thread(True, True, "2")
+
+    def test_run_with_gt_and_gen_3(self):
         self.__run_detector_thread(True, True, "3")
-        pass
 
 
 if __name__ == '__main__':
