11b58311a83bf984db5275b373f56f0798626e7e
dogc <caoyuqi1996@hotmail.com>
2023-03-16 05:16:19 +0100

    itest(integration_tests.testing_tools): added a basic class for selenium testing

---
+++ integration_tests/testing_tools.py
@@ -0,0 +1,38 @@
+import platform
+
+from django.conf import settings
+from selenium.webdriver.chrome.service import Service
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+
+
+class SeleniumTestCase(StaticLiveServerTestCase):
+    driver = None
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        options = webdriver.ChromeOptions()
+        options.add_argument("--start-maximized")
+        
+        # Automatically select the appropriate driver according to the system
+        os = platform.system().lower()
+        if "windows" in os:
+            service = Service(f"./driver/chromedriver_win32/chromedriver")
+        elif "linux" in os:
+            service = Service(f"./driver/chromedriver_linux64/chromedriver")
+        elif "darwin" in os:
+            if "x86" in platform.platform():
+                service = Service(f"./driver/chromedriver_mac64/chromedriver")
+            else:
+                service = Service(f"./driver/chromedriver_mac_arm64/chromedriver")
+        else:
+            return
+                
+        cls.driver = webdriver.Chrome(service=service, options=options)
+        cls.driver.implicitly_wait(10)
+        
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+        super().tearDownClass()
