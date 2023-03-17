01f35ac9d689817e8bcdb1eedece9a645f4d9678
Ao Zuo <unobm@student.kit.edu>
2023-03-17 16:38:47 +0100

    <itest> test_record_searching.py and test_record_sorting.py

--- integration_tests/TestCases/test_record_searching.py
+++ integration_tests/TestCases/test_record_searching.py
@@ -41,52 +41,11 @@ class test_searching(SeleniumTestCase):
         time.sleep(2)
         assert self.driver.current_url == self.live_server_url + "/main/"
         '''--------------------------- create experiment1 ---------------------------'''
-
-        # '''--------------------------- create experiment2 ---------------------------'''
-        # self.driver.find_element(By.ID, 'btnAdd').click()
-        # self.driver.implicitly_wait(10)
-        # self.driver.find_element(By.NAME, 'run_name').send_keys('test_exp2')
-        # self.driver.find_element(By.NAME, 'main_file').send_keys(
-        #     f"{settings.BASE_DIR}/integration_tests/Test_File/testcsv.csv")
-        # self.driver.find_element(By.ID, 'btnSave').click()
-        # time.sleep(2)
-        # # now in the conf page
-        # #assert self.driver.current_url == self.live_server_url + "/configuration/"
-        # # roll down the seit
-        # roll_down = "window.scrollTo(0, document.body.scrollHeight)"
-        # self.driver.execute_script(roll_down)
-        # self.driver.implicitly_wait(10)
-        # self.driver.implicitly_wait(10)
-        # # no other options, just use default setting
-        # self.driver.find_element(By.Value, "Add run to queue").click()
-        # time.sleep(2)
-        # assert self.driver.current_url == self.live_server_url + "/main/"
-        # '''--------------------------- create experiment2 ---------------------------'''
-        #
-        # '''--------------------------- create experiment3 ---------------------------'''
-        # self.driver.find_element(By.ID, 'btnAdd').click()
-        # self.driver.implicitly_wait(10)
-        # self.driver.find_element(By.NAME, 'run_name').send_keys('test_exp3')
-        # self.driver.find_element(By.NAME, 'main_file').send_keys(
-        #     f"{settings.BASE_DIR}/integration_tests/Test_File/testcsv.csv")
-        # self.driver.find_element(By.ID, 'btnSave').click()
-        # time.sleep(2)
-        # # now in the conf page
-        # #assert self.driver.current_url == self.live_server_url + "/configuration/"
-        # # roll down the seit
-        # roll_down = "window.scrollTo(0, document.body.scrollHeight)"
-        # self.driver.execute_script(roll_down)
-        # self.driver.implicitly_wait(10)
-        # self.driver.implicitly_wait(10)
-        # # no other options, just use default setting
-        # self.driver.find_element(By.Value, "Add run to queue").click()
-        # time.sleep(2)
-        # assert self.driver.current_url == self.live_server_url + "/main/"
-        # '''--------------------------- create experiment3 ---------------------------'''
         '''--------------------------- successful searching ---------------------------'''
-        #self.driver.find_element(By.CLASS_NAME, 'form-control.search-input').send_keys("test_exp1")
-
-
+        # self.driver.find_element(By.CLASS_NAME, 'form-control.search-input').send_keys("test_exp1")
+        # searched_exp = self.driver.find_element_by_css_selector('tr[data-index="0"]')
+        # assert searched_exp.text == 'test_exp1'
+        # self.driver.refresh()
         '''--------------------------- successful searching ---------------------------'''
 
         '''--------------------------- failed searching ---------------------------'''
